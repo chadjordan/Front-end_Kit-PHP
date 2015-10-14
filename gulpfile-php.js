06601722343ce6450599b324a5fa0689005e3520
Chad Jordan Hilis <chadjordan.hilis@gmail.com>
2015-10-15 07:22:49 +0800

    with markup

--- gulpfile-php.js
+++ gulpfile-php.js
@@ -10,7 +10,9 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var sass = require('gulp-sass');
-var browsersync = require('browser-sync').create();
+var connect = require('gulp-connect-php');
+var httpProxy = require('http-proxy');
+var browsersync = require('browser-sync');
 var reload = browsersync.reload;
 
 //custom path url
@@ -67,10 +69,27 @@ gulp.task('watch', function() {
     gulp.watch(SRC, ['sass']);
 });
 
-
 gulp.task('serve', ['sass'], function() {
-    browsersync.init({
-        server: "./application/"
+    connect.server({ base: 'application', port: 9001, keepalive: true, open: false});
+    
+    var proxy = httpProxy.createProxyServer({});
+    
+    browsersync({
+        notify: false,
+        port  : 9000,
+        server: {
+            baseDir   : ['application'],
+            
+            middleware: function (req, res, next) {
+                var url = req.url;
+
+                if (!url.match(/^\/(styles|fonts)\//)) {
+                    proxy.web(req, res, { target: 'http://127.0.0.1:9001' });
+                } else {
+                    next();
+                }
+            }
+        }
     });
     
     gulp.watch(['./application/assets/img/**/*'], reload);
@@ -83,6 +102,23 @@ gulp.task('serve', ['sass'], function() {
         }))
     
 });
+
+
+//gulp.task('serve', ['sass'], function() {
+//    browsersync.init({
+//        server: "./application/"
+//    });
+//    
+//    gulp.watch(['./application/assets/img/**/*'], reload);
+//    gulp.watch("./application/assets/js/**/*.js", ['jshint', 'jscompress', reload]);
+//    gulp.watch("./application/assets/scss/**/*.scss", ['sass', reload]);
+//    gulp.watch(['./application/**/*.html'], reload);
+//    gulp.watch(['./application/**/*.php'], reload);
+//    return gulp.on('error', notify.onError(function(error) {
+//            return "Gulp Error: " + error.message;
+//        }))
+//    
+//});
 
 gulp.task('copy', function() {
     return gulp.src([
--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,22 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var sass = require('gulp-sass');
-var connect = require('gulp-connect-php');
-var httpProxy = require('http-proxy');
-var browsersync = require('browser-sync');
+var browsersync = require('browser-sync').create();
 var reload = browsersync.reload;
 
 //custom path url
-var SRC = './application/assets/js/*.js';
+//var SRC = './application/assets/js/*.js';
 var DEST = 'production';
 
 
 gulp.task('sass', function(){
     gulp.src('./application/assets/scss/**/*.scss')
         .pipe(sass())
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
         .pipe(gulp.dest('./application/assets/css/dev-css'))
+    
 });
 
 gulp.task('imagemin', function() {
@@ -36,7 +38,6 @@ gulp.task('imagemin', function() {
 gulp.task('cssMinify', function() {
     return gulp.src('./application/assets/css/dev-css/*')
         .pipe(concat('main.min.css'))
-        .pipe(plumber())
         .pipe(cssMinify({
             keepSepecialComments: 1
         }))
@@ -67,58 +68,27 @@ gulp.task('jshint', function() {
 
 gulp.task('watch', function() {
     gulp.watch(SRC, ['sass']);
+    gulp.watch(SRC, ['cssMinify']);
 });
 
-gulp.task('serve', ['sass'], function() {
-    connect.server({ base: 'application', port: 9001, keepalive: true, open: false});
-    
-    var proxy = httpProxy.createProxyServer({});
-    
-    browsersync({
-        notify: false,
-        port  : 9000,
-        server: {
-            baseDir   : ['application'],
-            
-            middleware: function (req, res, next) {
-                var url = req.url;
 
-                if (!url.match(/^\/(styles|fonts)\//)) {
-                    proxy.web(req, res, { target: 'http://127.0.0.1:9001' });
-                } else {
-                    next();
-                }
-            }
-        }
+gulp.task('serve', ['sass', 'cssMinify'], function() {
+    browsersync.init({
+        server: "./application/"
     });
     
     gulp.watch(['./application/assets/img/**/*'], reload);
     gulp.watch("./application/assets/js/**/*.js", ['jshint', 'jscompress', reload]);
-    gulp.watch("./application/assets/scss/**/*.scss", ['sass', reload]);
+    gulp.watch("./application/assets/scss/**/*.scss", ['sass', 'cssMinify', reload]);
+    gulp.watch("./application/assets/css/*.*", ['cssMinify', reload]);
     gulp.watch(['./application/**/*.html'], reload);
     gulp.watch(['./application/**/*.php'], reload);
+    gulp.watch(['./application/*'], reload);
     return gulp.on('error', notify.onError(function(error) {
             return "Gulp Error: " + error.message;
         }))
     
 });
-
-
-//gulp.task('serve', ['sass'], function() {
-//    browsersync.init({
-//        server: "./application/"
-//    });
-//    
-//    gulp.watch(['./application/assets/img/**/*'], reload);
-//    gulp.watch("./application/assets/js/**/*.js", ['jshint', 'jscompress', reload]);
-//    gulp.watch("./application/assets/scss/**/*.scss", ['sass', reload]);
-//    gulp.watch(['./application/**/*.html'], reload);
-//    gulp.watch(['./application/**/*.php'], reload);
-//    return gulp.on('error', notify.onError(function(error) {
-//            return "Gulp Error: " + error.message;
-//        }))
-//    
-//});
 
 gulp.task('copy', function() {
     return gulp.src([
@@ -137,6 +107,6 @@ gulp.task('copy', function() {
 
 });
 
-gulp.task('default', ['serve', 'cssMinify', 'imagemin']);
+gulp.task('default', ['serve', 'imagemin']);
 
-gulp.task('prod', ['copy',  'imagemin']);
+gulp.task('prod', ['copy', 'imagemin']);
