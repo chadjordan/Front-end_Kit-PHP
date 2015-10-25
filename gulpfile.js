990600387f72e6c46b21b5b2bf509bc1752cea98
Chad Jordan Hilis <chadjordan.hilis@gmail.com>
2015-10-26 07:56:04 +0800

    modified and test gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,65 @@
 var gulp = require('gulp');
 
 //include plugins
-var jshint = require('gulp-jshint');
-var changed = require('gulp-changed');
-var plumber = require('gulp-plumber');
-var imagemin = require('gulp-imagemin');
-var cssMinify = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var uncss = require('gulp-uncss');
-var notify = require('gulp-notify');
-var sass = require('gulp-sass');
-var browsersync = require('browser-sync').create();
-var reload = browsersync.reload;
-var glob = require('glob');
+var jshint = require('gulp-jshint'),
+    changed = require('gulp-changed'),
+    plumber = require('gulp-plumber'),
+    imagemin = require('gulp-imagemin'),
+    cssMinify = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    uncss = require('gulp-uncss'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    browsersync = require('browser-sync').create(),
+    reload = browsersync.reload;
 
 //custom path url
-//var SRC = './application/assets/js/*.js';
-var DEST = 'production';
-
-
+var SRC = './application/assets/',
+    DEST = 'production';
 
 
 gulp.task('sass', function(){
-    gulp.src('./application/assets/scss/**/*.scss')
+    gulp.src(SRC + 'scss/**/*.scss')
         .pipe(sass())
         .on('error', notify.onError(function(error) {
             return "Gulp Error: " + error.message;
         }))
-        .pipe(gulp.dest('./application/assets/css/dev-css'))
-    
+        .pipe(gulp.dest(SRC + 'css/dev-css'))
 });
 
 gulp.task('imagemin', function() {
-    return gulp.src('./application/assets/img/**/*')
+    return gulp.src(SRC + 'img/**/*')
         .pipe(plumber())
-        .pipe(imagemin({ progressive: true, optimizationLevel: 5 }))
+        .pipe(imagemin({
+            progressive: true,
+            optimizationLevel: 5
+        }))
         .pipe(gulp.dest(DEST + '/assets/img/'));
 });
 
 gulp.task('cssMinify', function() {
-    return gulp.src('./application/assets/css/dev-css/*')
+    return gulp.src(SRC + 'css/dev-css/*')
         .pipe(concat('main.min.css'))
         .pipe(cssMinify({
             keepSepecialComments: 1
         }))
-        .pipe(gulp.dest('./application/assets/css'))
+        .pipe(gulp.dest(SRC + 'css'))
 });
-
 
 gulp.task('jscompress', function() {
-  return gulp.src('./application/assets/js/dev-js/*.js')
+  return gulp.src(SRC + 'js/dev-js/*.js')
     .pipe(concat('main.min.js')) //the name of the resulting file
     .pipe(uglify())
-    .pipe(gulp.dest('./application/assets/js'))
+    .pipe(gulp.dest(SRC + 'js'))
 });
 
-
 gulp.task('uncss', function () {
-    return gulp.src('./application/assets/css/main.min.css')
+    return gulp.src(SRC + 'css/main.min.css')
         .pipe(concat('main.min.css'))
         .pipe(uncss({
             html: ['./application/index.html'],
             ignore: [
-
-
                         /\.open/,
                          /(#|\.)fancybox(\-[a-zA-Z]+)?/,
                         /(#|\.)active(\-[a-zA-Z]+)?/,
@@ -82,7 +77,6 @@ gulp.task('uncss', function () {
                        // currently only in a IE conditional, so uncss doesn't see it
                         ".close",
                         ".alert-dismissible"
-
                     ]
         }))
         .pipe(gulp.dest(DEST + '/assets/dcss/'));
@@ -90,7 +84,7 @@ gulp.task('uncss', function () {
 
 
 gulp.task('jshint', function() {
-    gulp.src('./application/assets/js/main.js')
+    gulp.src(SRC + 'js/main.js')
         .pipe(plumber())
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
@@ -101,23 +95,20 @@ gulp.task('watch', function() {
     gulp.watch(SRC, ['cssMinify']);
 });
 
-
 gulp.task('serve', ['sass', 'cssMinify'], function() {
     browsersync.init({
         server: "./application/"
     });
-    
-    gulp.watch(['./application/assets/img/**/*'], reload);
-    gulp.watch("./application/assets/js/**/*.js", ['jshint', 'jscompress', reload]);
-    gulp.watch("./application/assets/scss/**/*.scss", ['sass', 'cssMinify', reload]);
-    gulp.watch("./application/assets/css/*.*", ['cssMinify', reload]);
+    gulp.watch([SRC + 'img/**/*'], reload);
+    gulp.watch(SRC + "js/**/*.js", ['jshint', 'jscompress', reload]);
+    gulp.watch(SRC + "scss/**/*.scss", ['sass', 'cssMinify', reload]);
+    gulp.watch(SRC + "css/*.*", ['cssMinify', reload]);
     gulp.watch(['./application/**/*.html'], reload);
     gulp.watch(['./application/**/*.php'], reload);
     gulp.watch(['./application/*'], reload);
     return gulp.on('error', notify.onError(function(error) {
             return "Gulp Error: " + error.message;
         }))
-    
 });
 
 gulp.task('copy', function() {
